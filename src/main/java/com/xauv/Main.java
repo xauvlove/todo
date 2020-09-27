edcc54f422a3f1b4cf62029937fbf90b3b975431
lingyue <xujinwei@youzan.com>
2020-09-27 16:19:54 +0800

    Initial commit

---
+++ src/main/java/com/xauv/Main.java
@@ -0,0 +1,17 @@
+package com.xauv;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+/**
+ * @Author ling yue
+ * @Date 2020/9/27 16:07
+ * @Desc description
+ */
+@Slf4j
+public class Main {
+    public static final ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) {
+
+    }
+}
